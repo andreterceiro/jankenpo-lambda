38e2c874033d48544efe1cb3081098df47a75987
André de Paula Terceiro <andre@terceiro.com.br>
2022-10-15 18:28:58 -0300

    All 1.0

---
+++ handler.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { O_TRUNC } = require("constants");
+
+module.exports.play = async (event) => {
+  return {
+    statusCode: 200,
+    body: process(event.queryStringParameters.choice)
+  };
+};
+
+/**
+ * Return who is the winner
+ * user GET parameter choice x computer random choice 
+ * 
+ * @return {string} A string representing the choice, like 'rock'
+ */
+function process(userChoice) {
+  if (userChoice != "paper" && userChoice != "rock" && userChoice != "scissors") {
+    return "Please provide your choice as GET parameter. Example: '?choice=paper' at the end of the URL"
+  }
+  const computerChoice = getComputerChoice();
+
+  if ((userChoice == "paper" && computerChoice == "rock") || (userChoice == "rock" && computerChoice == "scissors") || (userChoice == "scissors" && computerChoice == "paper")) {  
+    return `User win. Computer selected ${computerChoice}`;
+  } else if (userChoice == computerChoice) {
+    return `Draw. Computer selected ${computerChoice}`;
+  } else {
+    return `Computer win. Computer selected ${computerChoice}`;
+  }
+}
+
+/**
+ * Returns the computer choice
+ * 
+ * @return {string} A string representing the choice, like 'rock'
+ */
+function getComputerChoice() {
+  const choices = ["paper", "rock", "scissors"];
+  const randomComputerIndex = Math.floor(Math.random() * 3);
+  return choices[randomComputerIndex];
+}
